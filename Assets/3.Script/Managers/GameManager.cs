8f08523b67ce48f84881756386850615dd4130dd
So-ooming <tnals6254@naver.com>
2023-07-26 08:43:13 +0900

    [feat] 배경음 및 효과음 구현중

--- Assets/3.Script/Managers/GameManager.cs
+++ Assets/3.Script/Managers/GameManager.cs
@@ -28,23 +28,20 @@ public class GameManager : MonoBehaviour
     public int deathCnt = 0;
     public float timer = 0f;
     public Text coinText;
+    public Image whitePanel;
+    public Animator coinAnim;
 
     private void Start()
     {
         coinText = CoinUI.transform.GetChild(0).GetChild(0).GetComponent<Text>();
+        coinAnim = CoinUI.transform.GetComponent<Animator>();
     }
 
-    public void Ending()
-    {
-        LastDoor.transform.GetComponent<Animator>().enabled = true;
-        PlayerPrefs.SetInt("Coin", coinCnt);
-        PlayerPrefs.SetInt("Death", deathCnt);
-        PlayerPrefs.SetFloat("Time", timer);
-    }
 
     private void Update()
     {
-        timer += Time.deltaTime;
+        if(!isPause)
+            timer += Time.deltaTime;
         coinText.text = coinCnt.ToString();
         if (Input.GetKeyDown(KeyCode.Escape) && !PauseUI.activeSelf)
         {
@@ -60,6 +57,32 @@ public class GameManager : MonoBehaviour
         {
             BackPauseUI();
         }
+    }
+
+    public void Ending()
+    {
+        LastDoor.transform.GetComponent<Animator>().enabled = true;
+        PlayerPrefs.SetInt("Coin", coinCnt);
+        PlayerPrefs.SetInt("Death", deathCnt);
+        PlayerPrefs.SetFloat("Time", timer);
+
+        StartCoroutine("WhiteRust_co");
+    }
+
+    IEnumerator WhiteRust_co()
+    {
+        yield return new WaitForSeconds(6f);
+        for(int i = 0; i < 500; i++)
+        {
+            whitePanel.color += new Color(0, 0, 0, Time.deltaTime);
+            yield return null;
+        }
+        GoToEndingScene();
+    }
+
+    public void GoToEndingScene()
+    {
+        SceneManager.LoadScene("Ending");
     }
 
     public void ActivePauseUI()
